12024b6f0be47f3064a06825ddf0c3ab7342607b
John Paul Manalo <johnpauldeguzman.manalo29@gmail.com>
2025-02-02 23:20:34 +0800

    V2 may button na

--- resources/js/Components/Admin/ScholarsTabs/Adding.vue
+++ resources/js/Components/Admin/ScholarsTabs/Adding.vue
@@ -184,7 +184,11 @@
                         </div>
                         <input id="dropzone-file" type="file" class="hidden" accept=".csv" @change="(e) => handleFile(e.target.files[0])" @select="handleFileUpload"  />
                     </label>
-                    
+                    <div class="flex justify-end gap-2">
+                        <button type="submit" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                            Add Scholars
+                        </button>
+                    </div>
                     
                 </div> 
             </form>
