53e5a2a75924da540ca512cac4cfa0b6a805c475
John Paul Manalo <johnpauldeguzman.manalo29@gmail.com>
2025-03-17 23:24:27 +0800

    V4 sponsor

---
+++ resources/js/Pages/Sponsor/Dashboard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import ContentDashboard from '@/Pages/Staff/Dashboard/Content-Dashboard.vue';
+
+const props = defineProps({
+    scholarships: Array,
+    sponsors: Array,
+    scholars: Array,
+});
+
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #default>
+            <!-- Your dashboard content goes here -->
+            <ContentDashboard class="w-full h-full dark:bg-dprimary" :scholarships="scholarships" :sponsors="sponsors"/>
+        </template>
+    </AuthenticatedLayout>
+</template>
