6e72c488f78f681ee479838961b9b3e8f02ab241
Calvin-SoftEng <116782457+Calvin-SoftEng@users.noreply.github.com>
2025-04-21 01:13:37 +0800

    V8

--- routes/web.php
+++ routes/web.php
@@ -45,9 +45,9 @@ Route::get('/', [LandingPageController::class, 'index'])->name('welcome');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // Notification routes
     Route::get('/notifications', [NotificationController::class, 'index']);
