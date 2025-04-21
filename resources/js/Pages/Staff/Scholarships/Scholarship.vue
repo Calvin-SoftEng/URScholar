15f35e7c572c9495b195480c39f4eb7b5286df18
Calvin-SoftEng <116782457+Calvin-SoftEng@users.noreply.github.com>
2025-04-22 02:07:22 +0800

    v8

--- resources/js/Pages/Staff/Scholarships/Scholarship.vue
+++ resources/js/Pages/Staff/Scholarships/Scholarship.vue
@@ -1580,7 +1580,7 @@
                         </div> -->
 
                         <!-- Forward Button -->
-                        <div v-if="completedBatches === batches.length || accomplishedBatches" class="mt-4">
+                        <div v-if="completedBatches === batches.length && AllvalidationStatus || accomplishedBatches" class="mt-4">
                             <button type="submit" :disabled="isSubmitting || selectedBatches.length === 0"
                                 @click="forwardSponsor"
                                 class="w-full bg-blue-600 hover:bg-blue-700 text-white font-medium py-2.5 px-4 rounded-lg transition duration-200 disabled:opacity-50 disabled:cursor-not-allowed">
